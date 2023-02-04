071b3dfad24932631f0d3bfb4419fd94ff227f30
anna97490 <anna.cousin97490@gmail.com>
2023-02-04 23:21:44 +0100

    Creation of the pages Login Signin Dashboard and the routes

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'index', component: IndexComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { ButtonComponent } from './components/button/button.component';
 import { IndexComponent } from './index/index.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
 
 @NgModule({
@@ -16,6 +19,9 @@ import { IndexComponent } from './index/index.component';
     HomeComponent,
     ButtonComponent,
     IndexComponent,
+    DashboardComponent,
+    LoginComponent,
+    SigninComponent,
   ],
   imports: [
     BrowserModule,
