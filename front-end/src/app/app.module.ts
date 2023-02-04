4ee781eee89548cfb74f80be9a97101e2c441588
anna97490 <anna.cousin97490@gmail.com>
2023-02-04 14:10:14 +0100

    clean branches

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ButtonComponent } from './components/button/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ButtonComponent,
 
   ],
   imports: [
