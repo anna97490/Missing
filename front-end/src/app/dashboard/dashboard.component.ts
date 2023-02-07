870a93920ffa084637d00d81365f311226212f5e
anna97490 <anna.cousin97490@gmail.com>
2023-02-07 12:33:17 +0100

    tailwing added and style of the front started

--- front-end/src/app/dashboard/dashboard.component.ts
+++ front-end/src/app/dashboard/dashboard.component.ts
@@ -1,47 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
   tabs: HTMLElement[] = [];
   content: HTMLElement[] = [];
   index: any;
 
-  ngOnInit() {
-    this.tabs = Array.from(document.querySelectorAll('.tabs'));
-    this.content = Array.from(document.querySelectorAll('.content'));
-    this.index = 0;
 
-    this.tabs.forEach(tab => {
-      tab.addEventListener('click', () => {
-
-        if (tab.classList.contains('active')) {
-          return;
-        } else {
-          tab.classList.add('active');
-        }
-
-        this.index = tab.getAttribute('data-dashboard');
-
-        for (let i = 0; i < this.tabs.length; i++) {
-
-          if (this.tabs[i].getAttribute('data-dashboard') !== this.index) {
-            this.tabs[i].classList.remove('active');
-          }
-        }
-
-        for (let j = 0; j < this.content.length; j++) {
-
-          if (this.content[j].getAttribute('data-dashboard') === this.index) {
-            this.content[j].classList.add('activeContent');
-          } else {
-            this.content[j].classList.remove('activeContent');
-          }
-        }
-      });
-    });
-  }
 }
